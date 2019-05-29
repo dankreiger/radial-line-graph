eb588fd125904264dec7281c4df5a05b592fb724
Dan Kreiger <dan@dankreiger.com>
2019-05-29 09:30:13 +0200

    fix(data): fix dataset length

--- data/sampleData.js
+++ data/sampleData.js
@@ -1,4 +1,4 @@
 export const dataPoints = 10;
 export const puppies = [13, 9, 12, 13, 7, 8, 1, 7, 5, 14];
 export const cats = [3, 3, 9, 2, 15, 16, 18, 12, 15, 2];
-export const corporations = [14, 2, 12, 2, 10, 1, 6, 4, 16, 15];
+export const corporations = [14, 2, 4, 2, 10, 1, 6, 4, 5, 15];
