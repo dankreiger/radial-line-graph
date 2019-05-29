10e440a1df275397b850947a55bdcac6cab5a8ad
Dan Kreiger <dan@dankreiger.com>
2019-05-29 03:58:55 +0200

    feat(road): small enhancesments

--- data/sampleData.js
+++ data/sampleData.js
@@ -1,4 +1,4 @@
 export const dataPoints = 10;
 export const puppies = [13, 9, 12, 13, 7, 8, 1, 7, 5, 17, 8];
 export const cats = [20, 3, 9, 2, 15, 16, 18, 12, 1, 17, 5];
-export const corporations = [8, 16, 5, 2, 10, 1, 6, 4, 19, 11, 18];
+export const corporations = [8, 16, 5, 2, 10, 1, 6, 4, 16, 11, 15];
